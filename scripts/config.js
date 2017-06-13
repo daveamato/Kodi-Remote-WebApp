47452db6723f2e139065d7123b890af38fdd351b
David Amato <daveamato@live.com>
2017-06-13 15:35:51 -0400

    initial commit

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var config = angular.module('kodiConfig', [])
-  .constant('KODI_URL', "192.168.1.115")
+  .constant('KODI_URL', window.KODI_IP || "192.168.1.247")
   .constant('SOCKET_TIMEOUT', 200)
   .constant('PER_PAGE', 100)
-  .constant('DEBUG', false);
+  .constant('DEBUG', false)
+;
